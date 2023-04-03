033613fdef94e76899507264b4e9325291bde6f5
Umar Baihaqi <umar.baihaqi64@gmail.com>
2023-04-03 17:55:01 +0700

    guide

---
+++ app/Http/Controllers/GuideController.php
@@ -0,0 +1,129 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Guide;
+use Illuminate\Http\Request;
+
+class GuideController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return response()->json([
+            "message" => "success",
+            "statusCode" => 200,
+            "data" => Guide::all(),
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        try {
+            $isValidateData = $request->validate([
+                "name" => 'required',
+            ]);
+            Guide::create($isValidateData);
+            return response()->json([
+                "message" => "success",
+                'statusCode' => 200,
+                "data" => $isValidateData,
+            ]);
+        } catch (\Throwable $th) {
+            return response()->json([
+                "message" => $th->getMessage(),
+                'statusCode' => 400,
+                "data" => null
+            ]);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Guide  $guide
+     * @return \Illuminate\Http\Response
+     */
+   
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Guide  $guide
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Guide $guide)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Guide  $guide
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Guide $guide)
+    {
+        try {
+            $isValidateData = $request->validate([
+                "name" => 'required',
+            ]);
+            $guide->update($isValidateData);
+            return response()->json([
+                "message" => "success",
+                'statusCode' => 200,
+                "data" => $isValidateData,
+            ]);
+        } catch (\Throwable $th) {
+            return response()->json([
+                "message" => $th->getMessage(),
+                'statusCode' => 400,
+                "data" => null
+            ]);
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Guide  $guide
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Guide $guide)
+    {
+        try {
+            $guide->delete();
+            return response()->json([
+                "message" => "success",
+                'statusCode' => 200,
+            ]);
+        } catch (\Throwable $th) {
+            return response()->json([
+                "message" => $th->getMessage(),
+                'statusCode' => 400,
+            ]);
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\GuideController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('guide', [GuideController::class, 'store']);
+Route::get('guide', [GuideController::class, 'index']);
+Route::put('guide/{id}', [GuideController::class, 'update']);
+Route::delete('guide/{id}', [GuideController::class, 'destroy']);
